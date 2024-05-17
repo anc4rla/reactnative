5b7631606cfb25420025956ad1bc5e111083b3ec
jebsdev <159553141+anc4rla@users.noreply.github.com>
2024-05-17 11:57:12 -0300

    gitem
    git novinho q dlc

--- appanacarla/src/screens/index.tsx
+++ appanacarla/src/screens/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput} from 'react-native';
 import {styles} from './styles'
 export function SignIn(){
+  const [text,setText] = useState ("Você não digitou nada ainda");
 
   return(
     <View style={styles.container}>
       <Text>hello world!</Text>
-      <TextInput style={styles.input}/>
+
+      <TextInput style={styles.input} onChangeText={setText}/>
+      
+      <Text>
+        você digitou: {text}
+      </Text>
     </View>
   );
 
