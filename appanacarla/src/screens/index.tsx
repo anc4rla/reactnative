e0e6cbac43172007cbd2722165ab27162077278a
evellynamf <159553141+anc4rla@users.noreply.github.com>
2024-05-24 11:54:27 -0300

    gitem mores gitem
    oioioioi lalalalalalala gitando

--- appanacarla/src/screens/index.tsx
+++ appanacarla/src/screens/index.tsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Image} from 'react-native';
 import {styles} from './styles'
-import IllustrationImg from '../assets/IllustrationImg.png';
-<div styleName={} />
-<source />assets'
+import IllustrationImg from '../assets/illustration.png'
+import { StatusBar } from 'expo-status-bar';
 export function SignIn(){
+
   const [text,setText] = useState ("Você não digitou nada ainda");
 
   return(
     <View style={styles.container}>
-      <Image source={} />
+    <StatusBar barStyle='light-content' backgroundColor={'transparent'} translucent />
+      <Image source={IllustrationImg} style={styles.image} resizeMode='stretch'/>
+    <View style ={styles.content}>
+
+      <Text style={styles.title}>
+        Organize suas {'\n'}
+        Jogatinas {'\n'}
+        Facilmente
+      </Text>
+      <Text style={styles.subtitle}>
+        Crie grupos para jogar seus games{'\n'}
+        favorito com seus amigos
+      </Text>
+
+    </View>
     </View>
   );
 
