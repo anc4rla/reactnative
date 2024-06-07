eaf6e64900f1b0e66cbd709563e40f98cf57e07d
MariaKlara8D <159553141+anc4rla@users.noreply.github.com>
2024-06-07 11:54:38 -0300

    git
    ana

--- appanacarla/src/screens/styles.ts
+++ appanacarla/src/screens/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor: 'purple',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center'
     },
